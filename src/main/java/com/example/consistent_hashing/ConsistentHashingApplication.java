9dff40b9884e8f83e4930b6aa973c241bc7da554
Nidhi Jain <nidhijain@Nidhis-MacBook-Air.local>
2026-02-18 16:54:07 +0530

    Springboot project init

---
+++ src/main/java/com/example/consistent_hashing/ConsistentHashingApplication.java
@@ -0,0 +1,13 @@
+package com.example.consistent_hashing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsistentHashingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsistentHashingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/consistent_hashing/ConsistentHashingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consistent_hashing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsistentHashingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
